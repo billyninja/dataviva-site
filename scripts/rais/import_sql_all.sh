8a4a674f4dd3ce3f9a50ec051ffad2593e2c4d34
Alex Simoes <alexandersimoes@gmail.com>
2013-10-30 13:56:29 -0400

    scripts to run import for all years all tables

---
+++ scripts/rais/import_sql_all.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+years=(2002 2003 2004 2005 2006 2007 2008 2009 2010 2011)
+tables=(yb ybi ybio ybo yi yio yo)
+
+for year in ${years[*]}
+do
+  for table in ${tables[*]}
+  do
+    bash $DIR/import_sql.sh $year $table
+  done
+done
