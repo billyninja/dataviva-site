7a4d88b4dae51f4517858a500ced36e1629613ae
Alex Simoes <alexandersimoes@gmail.com>
2013-10-31 16:16:46 -0400

    update order of YBIO columns for import in years > 2002

--- scripts/rais/import_sql.sh
+++ scripts/rais/import_sql.sh
@@ -4,11 +4,11 @@ if [ $2 = "yb" ]; then
   file=yb_diversity.tsv.bz2
   fields=(bra_id year wage num_emp num_est wage_avg num_emp_est isic_diversity isic_diversity_eff cbo_diversity cbo_diversity_eff)
   if [ $1 -gt "2002" ]; then
-    file=yb_uniques_growth.tsv.bz2
+    file=yb_diversity_growth.tsv.bz2
     fields=(bra_id year wage num_emp num_est wage_avg num_emp_est isic_diversity isic_diversity_eff cbo_diversity cbo_diversity_eff wage_growth_val wage_growth_pct num_emp_growth_val num_emp_growth_pct)
   fi
   if [ $1 -gt "2006" ]; then
-    file=yb_uniques_growth.tsv.bz2
+    file=yb_diversity_growth.tsv.bz2
     fields=(bra_id year wage num_emp num_est wage_avg num_emp_est isic_diversity isic_diversity_eff cbo_diversity cbo_diversity_eff wage_growth_val wage_growth_pct num_emp_growth_val num_emp_growth_pct wage_growth_val_5 wage_growth_pct_5 num_emp_growth_val_5 num_emp_growth_pct_5)
   fi
 fi
@@ -18,11 +18,11 @@ if [ $2 = "yi" ]; then
   file=yi_diversity.tsv.bz2
   fields=(isic_id year wage num_emp num_est wage_avg num_emp_est cbo_diversity cbo_diversity_eff bra_diversity bra_diversity_eff)
   if [ $1 -gt "2002" ]; then
-    file=yi_uniques_growth.tsv.bz2
+    file=yi_diversity_growth.tsv.bz2
     fields=(isic_id year wage num_emp num_est wage_avg num_emp_est cbo_diversity cbo_diversity_eff bra_diversity bra_diversity_eff wage_growth_val wage_growth_pct num_emp_growth_val num_emp_growth_pct)
   fi
   if [ $1 -gt "2006" ]; then
-    file=yi_uniques_growth.tsv.bz2
+    file=yi_diversity_growth.tsv.bz2
     fields=(isic_id year wage num_emp num_est wage_avg num_emp_est cbo_diversity cbo_diversity_eff bra_diversity bra_diversity_eff wage_growth_val wage_growth_pct num_emp_growth_val num_emp_growth_pct wage_growth_val_5 wage_growth_pct_5 num_emp_growth_val_5 num_emp_growth_pct_5)
   fi
 fi
@@ -32,11 +32,11 @@ if [ $2 = "yo" ]; then
   file=yo_diversity.tsv.bz2
   fields=(cbo_id year wage num_emp num_est wage_avg num_emp_est isic_diversity isic_diversity_eff bra_diversity bra_diversity_eff)
   if [ $1 -gt "2002" ]; then
-    file=yo_uniques_growth.tsv.bz2
+    file=yo_diversity_growth.tsv.bz2
     fields=(cbo_id year wage num_emp num_est wage_avg num_emp_est isic_diversity isic_diversity_eff bra_diversity bra_diversity_eff wage_growth_val wage_growth_pct num_emp_growth_val num_emp_growth_pct)
   fi
   if [ $1 -gt "2006" ]; then
-    file=yo_uniques_growth.tsv.bz2
+    file=yo_diversity_growth.tsv.bz2
     fields=(cbo_id year wage num_emp num_est wage_avg num_emp_est isic_diversity isic_diversity_eff bra_diversity bra_diversity_eff wage_growth_val wage_growth_pct num_emp_growth_val num_emp_growth_pct wage_growth_val_5 wage_growth_pct_5 num_emp_growth_val_5 num_emp_growth_pct_5)
   fi
 fi
@@ -92,11 +92,11 @@ if [ $2 = "ybio" ]; then
   fields=(year bra_id isic_id cbo_id wage num_emp num_est wage_avg num_emp_est required)
   if [ $1 -gt "2002" ]; then
     file=ybio_required_growth.tsv.bz2
-    fields=(isic_id cbo_id year wage num_emp num_est wage_avg num_emp_est required wage_growth_val wage_growth_pct num_emp_growth_val num_emp_growth_pct)
+    fields=(bra_id isic_id cbo_id year wage num_emp num_est wage_avg num_emp_est required wage_growth_val wage_growth_pct num_emp_growth_val num_emp_growth_pct)
   fi
   if [ $1 -gt "2006" ]; then
     file=ybio_required_growth.tsv.bz2
-    fields=(isic_id cbo_id year wage num_emp num_est wage_avg num_emp_est required wage_growth_val wage_growth_pct num_emp_growth_val num_emp_growth_pct wage_growth_val_5 wage_growth_pct_5 num_emp_growth_val_5 num_emp_growth_pct_5)
+    fields=(bra_id isic_id cbo_id year wage num_emp num_est wage_avg num_emp_est required wage_growth_val wage_growth_pct num_emp_growth_val num_emp_growth_pct wage_growth_val_5 wage_growth_pct_5 num_emp_growth_val_5 num_emp_growth_pct_5)
   fi
 fi
 
@@ -123,5 +123,11 @@ fi
 bunzip2 -k $file
 file=${file%".bz2"}
 echo $file
-mysql -u $DATAVIVA_DB_USER -p -e "load data local infile '$file' into table rais_$2 fields terminated by '\t' lines terminated by '\n' IGNORE 1 LINES $sql_fields SET $sql_set" dataviva --local-infile=1
+
+if [[ -z $DATAVIVA_DB_PW ]]; then
+  mysql -u $DATAVIVA_DB_USER -e "load data local infile '$file' into table rais_$2 fields terminated by '\t' lines terminated by '\n' IGNORE 1 LINES $sql_fields SET $sql_set" dataviva --local-infile=1
+else
+  mysql -u $DATAVIVA_DB_USER -p $DATAVIVA_DB_PW -e "load data local infile '$file' into table rais_$2 fields terminated by '\t' lines terminated by '\n' IGNORE 1 LINES $sql_fields SET $sql_set" dataviva --local-infile=1
+fi
+
 rm $file
